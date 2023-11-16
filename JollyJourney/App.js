bb5d885392e9721fee7f2a4962fd548ff4da18b2
lyvia <lyviasemaoune@gmail.com>
2023-11-16 11:51:57 +0100

    firebase

--- JollyJourney/App.js
+++ JollyJourney/App.js
@@ -1,33 +1,52 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './app/screens/Login';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { onAuthStateChanged } from 'firebase/auth';
+import { FIREBASE_AUTH } from './FirebaseConfig';
+import { User } from 'firebase/auth';
+
+
+
+const Stack = createNativeStackNavigator();
+const InsideStack = createNativeStackNavigator();
+
+// function InsideLayout() {
+//   return(
+//     <InsideStack.Navigator>
+//       <InsideStack.Screen name="Inside" component={Inside} />
+//     </InsideStack.Navigator>
+//   )
+// }
 
 export default function App() {
 
-  const [message, setmessage] = useState('')
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    fetch('https://jolly-journey-3o4dnumtc-lyvias-projects.vercel.app/api/message')
-    .then((response) => response.json())
-    .then((data) => setmessage(data.message))
-    .catch((error) => console.error('Erreur:', error));
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      console.log('user ', user);
+      setUser(user);
+    })
   }, [])
 
-
+  const Stack = createNativeStackNavigator()
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <Text>{message}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='Login' component={Login} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ JollyJourney/FirebaseConfig.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBItoAJai-7egCzVcG1kPBNr59It0bHajo",
+  authDomain: "jolly-journey-680ea.firebaseapp.com",
+  projectId: "jolly-journey-680ea",
+  storageBucket: "jolly-journey-680ea.appspot.com",
+  messagingSenderId: "204091247098",
+  appId: "1:204091247098:web:936c6c43500b6731783c43"
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
