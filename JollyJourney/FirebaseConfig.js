18e1d6625eeae02f19c03eedd3bf8f7dc080e13a
lyvia <lyviasemaoune@gmail.com>
2023-12-14 23:51:09 +0100

    Fonctionnalité search

--- JollyJourney/FirebaseConfig.js
+++ JollyJourney/FirebaseConfig.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from '@firebase/firestore';
+import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +20,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const firestore = getFirestore(FIREBASE_APP);
+//setPersistence(getAuth(), browserLocalPersistence);
